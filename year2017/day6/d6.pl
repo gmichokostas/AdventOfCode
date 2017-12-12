184c59baa30e299b985a1cac28befcbd34fb91ac
George Michokostas <michokostas@gmail.com>
2017-12-12 14:01:10 +0100

    add perl solution for day 6 (brute force)

---
+++ year2017/day6/d6.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature qw(say);
+
+my @banks = split(" ", <STDIN>);
+
+sub maxindex {
+  my $arr_ref = shift;
+  my $max_index = 0;
+  for my $index (0.. scalar(@$arr_ref) - 1) {
+    if (($arr_ref->[$index] > $arr_ref->[$max_index]) &&
+        $arr_ref->[$index] != $arr_ref->[$max_index]) {
+      $max_index = $index;
+    }
+  }
+
+  return $max_index;
+}
+
+my $redistribution = 0;
+my %seen_arrays = (join("", @banks) => [1, 0]);
+
+while (1) {
+  my $idx = maxindex(\@banks);
+  my $blocks = $banks[$idx];
+
+  $banks[$idx] = 0;
+  $redistribution++;
+
+  while ($blocks != 0) {
+    $blocks--;
+    $idx++;
+    if ($idx > $#banks) { $idx = 0; }
+    $banks[$idx] += 1;
+  }
+
+  $seen_arrays{join("", @banks)}->[0] += 1;
+  for my $key (keys %seen_arrays) {
+    $seen_arrays{$key}->[1] += 1;
+  }
+
+  if ($seen_arrays{join("", @banks)}->[0] > 1) {
+    say $seen_arrays{join("", @banks)}->[1]-1; # print circles
+    last;
+  }
+
+}
+
+say $redistribution;
